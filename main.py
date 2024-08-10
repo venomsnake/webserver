ef1ee6b0811321b9049c9e318d6a4674de6f64b4
venomsnake <thakurmark07@gmail.com>
2024-08-10 12:46:12 +0530

    BodyReader & Split instead of Index

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """
 Refrence = https://defn.io/2018/02/25/web-app-from-scratch-01/
+2018/03/04
+2018/03/20
 
 """
 
@@ -7,6 +9,8 @@ import socket
 import typing
 import mimetypes
 import os
+from collections import defaultdict
+import io
 
 HOST = "localhost"
 PORT = 9000
@@ -85,7 +89,7 @@ def serve_file(sock: socket.socket, path: str) -> None:
         return
 
 # Request abstraction read files off disk
-
+'''
 def iter_lines(sock: socket.socket, bufsize: int = 16_384) -> typing.Generator[bytes, None, bytes]:
     #read all lines until EOL
 
@@ -108,14 +112,84 @@ def iter_lines(sock: socket.socket, bufsize: int = 16_384) -> typing.Generator[b
                 yield line
             except IndexError:
                 break
+            '''
+def iter_lines(sock: socket.socket, bufsize: int = 16_384) -> typing.Generator[bytes, None, bytes]:
+    buff = b""
+    while True:
+        data = sock.recv(bufsize)
+        if not data:
+            if buff:
+                # Process remaining buffer if there's data left
+                if b"\r\n" in buff:
+                    # Find the position of the last line break
+                    while b"\r\n" in buff:
+                        i = buff.find(b"\r\n")
+                        line, buff = buff[:i], buff[i + 2:]
+                        if line:
+                            yield line
+                if buff:
+                    yield buff
+            return
+
+        buff += data
+        # Process complete lines in the buffer
+        while b"\r\n" in buff:
+            i = buff.find(b"\r\n")
+            line, buff = buff[:i], buff[i + 2:]
+            if line:
+                yield line
 
 
-# Request  Class definition
+# Header class definition: To handle muliple values for single header
+
+class Headers:
+    def __init__(self) -> None:
+        self._headers = defaultdict(list)
+
+    def add(self, name: str, value: str) -> None:
+        self._headers[name.lower()].append(value)
+
+    def get_all(self, name: str) -> typing.List[str]:
+        return self._headers[name.lower()]
+    
+    def get(self, name: str, default: typing.Optional[str] = None) -> typing.Optional[str]:
+        try:
+            return self.get_all(name)[-1]
+        except IndexError:
+            return default
+
+
+# BodyReader class definition
+
+class BodyReader(io.IOBase):
+    def __init__(self, sock: socket.socket, *, buff: bytes = b"", bufsize: int = 16_384) -> None:
+        self._sock = sock
+        self._buff = buff
+        self._bufsize = bufsize
+
+    def readable(self) -> bool:
+        return True
+
+    def read(self, n: int) -> bytes:
+        """Read up to n number of bytes from the request body.
+        """
+        while len(self._buff) < n:
+            data = self._sock.recv(self._bufsize)
+            if not data:
+                break
+
+            self._buff += data
+
+        res, self._buff = self._buff[:n], self._buff[n:]
+        return res
+
+# Request Class definition
 
 class Request(typing.NamedTuple):
     method: str
     path: str
-    headers: typing.Mapping[str, str]
+    headers: Headers
+    body: BodyReader
 
     @classmethod
     def from_socket(cls, sock: socket.socket) -> "Request":
@@ -136,15 +210,25 @@ class Request(typing.NamedTuple):
         except ValueError:
             raise ValueError(f"Malformed request line {request_line!r}.")
 
-        headers = {}
-        for line in lines:
+        headers = Headers()
+        buff = b""
+        while True:
+            try:
+                line = next(lines)
+            except StopIteration as e:
+                # StopIteration.value contains the return value of the generator.
+                buff = e.value
+                break
+
             try:
                 name, _, value = line.decode("ascii").partition(":")
-                headers[name.lower()] = value.lstrip()
+                headers.add(name, value.lstrip())
             except ValueError:
                 raise ValueError(f"Malformed header line {line!r}.")
 
-        return cls(method=method.upper(), path=path, headers=headers)
+        body = BodyReader(sock, buff=buff)
+        return cls(method=method.upper(), path=path, headers=headers, body=body)
+
 
 
 with socket.socket() as server_sock:
@@ -175,6 +259,16 @@ with socket.socket() as server_sock:
             #for reqest_line in iter_lines(client_sock):print(request_line)
             try:
                 request = Request.from_socket(client_sock)
+                print(f"Received request: {request.method} {request.path}")
+                try:
+                    content_length = int(request.headers.get("content-length", "0"))
+                except ValueError:
+                    content_length = 0
+
+                if content_length:
+                    body = request.body.read(content_length)
+                    print("Request body", body)
+
                 if request.method != "GET":
                     client_sock.sendall(METHOD_NOT_ALLOWED_RESPONSE)
                     continue
